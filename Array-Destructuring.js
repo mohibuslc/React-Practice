2f29d084d48eb6beed27a4eee5506c17673bdf7f
mohibuslc <mohib_uslc@outlook.com>
2021-03-03 23:05:25 +0000

    Array-Function

--- Array-Destructuring.js
+++ Array-Destructuring.js
@@ -4,10 +4,30 @@
 
 
 
-const numbers = [45, 98];
+//const numbers = [45, 98];
 
 
-const x = numbers[0];
-const y = numbers[1];
+// const x = numbers[0];
+// const y = numbers[1];
 
-console.log(x,y);
+// console.log(x,y);
+
+// function :
+
+
+function TestNumber(num1){
+
+return [num1 , num1+5];
+}
+//-------------------------------------------------
+// One Array :
+
+//================================.
+//const Numbers = TestNumber(8);
+//console.log(Numbers);
+//------------------------------------------------------------------
+// if you want use 2 Array:
+
+const [Numbers1 , Numbers2]= TestNumber(8);
+
+console.log(Numbers1, Numbers2);
