c8624a1f267f3ac12ee5144b366af45414578d20
mohibuslc <mohib_uslc@outlook.com>
2021-03-04 23:26:10 +0000

    30.5 Lesson end :

---
+++ 4-js-shortcut.js
@@ -0,0 +1,29 @@
+// const cssClass = active ? 'Active ' : 'Inactive';
+
+// console.log(cssClass);
+
+let  money = 10;
+
+// let food ;
+
+// if ( money > 100 ){
+
+//     food = ' Briany';
+// }
+
+// else{
+
+//     food = ' Dall+Vath';
+
+// }
+
+// console.log( food);
+
+
+// Shortcut Command in JS : Call this process : TARNARY : 
+
+
+let food = money > 100 ? 'Brayni' : ' Alu + Borta';
+
+console.log (food);
+
---
+++ Object_Des.js
@@ -0,0 +1,18 @@
+const person = { Id: 1001, Name : 'Rahim', Age: 67, Salary: 34000,};
+
+
+const {Name , Age , Salary} = person;
+
+console.log(Name , Age,Salary);
+
+
+
+// create Object Shortcut:
+
+
+const x = 34;
+const y = 90;
+
+const obj = { x, y};
+
+console.log(obj);
