943325930bd8b58ce9319d15c53079d98b574ae8
mohibuslc <mohib_uslc@outlook.com>
2021-03-05 23:24:26 +0000

    JSON

---
+++ JSON_30_5-5.js
@@ -0,0 +1,27 @@
+// JSON:
+
+
+const info ={
+
+
+    name :'Abdul',
+
+    age : 75,
+
+    salary : 10000,
+
+    Address:  'Uposhohor Sylhet '
+
+}
+
+
+console.log(JSON.stringify(info));
+
+// JSON Data to convert 
+
+
+// const data = JSON.stringify(info);
+
+// const ConvertDaTa = JSON.parse(data);
+
+// console.log(ConvertDaTa.salary);
